b78d94932d80f610c0549abaa6af3c6e00cf1488
Guohui Xiao <gh.xiao@gmail.com>
2013-01-25 20:49:55 +0100

    s/ans/Q/g

--- scripts/sparql2cq.sh
+++ scripts/sparql2cq.sh
@@ -9,10 +9,10 @@ cat $1 | \
 grep -v "^#" | \
 sed "s/^WHERE.*$/<-/g" | \
 grep -v 'PREFIX' | \
-sed "s/SELECT\s\+$VAR\s\+$VAR\s\+$VAR\s\+$VAR/ans(?\1, ?\2, ?\3, ?\4)/g" | \
-sed "s/SELECT\s\+$VAR\s\+$VAR\s\+$VAR/ans(?\1,?\2,?\3)/g" | \
-sed "s/SELECT\s\+$VAR\s\+$VAR/ans(?\1,?\2)/g" | \
-sed "s/SELECT\s\+$VAR/ans(?\1)/g" | \
+sed "s/SELECT\s\+$VAR\s\+$VAR\s\+$VAR\s\+$VAR/Q(?\1, ?\2, ?\3, ?\4)/g" | \
+sed "s/SELECT\s\+$VAR\s\+$VAR\s\+$VAR/Q(?\1,?\2,?\3)/g" | \
+sed "s/SELECT\s\+$VAR\s\+$VAR/Q(?\1,?\2)/g" | \
+sed "s/SELECT\s\+$VAR/Q(?\1)/g" | \
 sed "s/\s*$VAR\s\+a\s\+$ID:\($ID\)\s\+./\2(?\1),/g" | \
 sed "s/\s*$VAR\s\+$ID:\($ID\)\s\+$VAR\s\+./\2(?\1,?\3),/g" | \
 sed "s/}//g" | \
